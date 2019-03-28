b9e9c12c1b906b1618e58fd5f64f4d1520c5c122
swissarmytowel <swissarmytowel@gmail.com>
2019-03-28 18:56:55 +0300

    Add Three-Address Code node

---
+++ Compilers/ThreeAddressCode/TacNodes/TacNode.cs
@@ -0,0 +1,40 @@
+namespace SimpleLang.ThreeAddressCode.TacNodes
+{
+    public class TacNode
+    {
+        public string Label { get; set; }
+        public string RightPart { get; set; }
+        public string FirstOperand { get; set; }
+        public string SecondOperand { get; set; }
+        public string Operation { get; set; }
+
+        private bool Equals(TacNode other)
+        {
+            return string.Equals(Label, other.Label) 
+                   && string.Equals(RightPart, other.RightPart) 
+                   && string.Equals(FirstOperand, other.FirstOperand) 
+                   && string.Equals(SecondOperand, other.SecondOperand) 
+                   && string.Equals(Operation, other.Operation);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((TacNode) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Label != null ? Label.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (RightPart != null ? RightPart.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (FirstOperand != null ? FirstOperand.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (SecondOperand != null ? SecondOperand.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Operation != null ? Operation.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+}
