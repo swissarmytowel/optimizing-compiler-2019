becad6aa8971ea4150efce9f17e3b743ac37d009
notenot <notsu@mail.ru>
2019-04-10 12:26:15 +0300

    Add CFG visualization with ToString

--- Compilers/CFG/ControlFlowGraph.cs
+++ Compilers/CFG/ControlFlowGraph.cs
@@ -2,11 +2,53 @@
 using SimpleLang.TACode;
 using QuickGraph;
 using QuickGraph.Graphviz;
-using QuickGraph.Graphviz.Dot;
 
 namespace SimpleLang.CFG
 {
     public class ControlFlowGraph
     {
+        private BasicBlocks _blocks;
+        private AdjacencyGraph<ThreeAddressCode, Edge<ThreeAddressCode>> _graph;
+        public BasicBlocks Blocks => _blocks;
+        public AdjacencyGraph<ThreeAddressCode, Edge<ThreeAddressCode>> Graph => _graph;
+
+        public ControlFlowGraph()
+        {
+            _graph = new AdjacencyGraph<ThreeAddressCode, Edge<ThreeAddressCode>>();
+        }
+
+        public void BuildFrom(ThreeAddressCode tac)
+        {
+            _graph.Clear();
+            _blocks = new BasicBlocks();
+            _blocks.SplitTACode(tac);
+
+            Build();
+        }
+
+        public void BuildFrom(BasicBlocks blocks)
+        {
+            _graph.Clear();
+            _blocks = blocks;
+
+            Build();
+        }
+
+        private void Build()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            var viz = new GraphvizAlgorithm<ThreeAddressCode, Edge<ThreeAddressCode>>(_graph);
+            viz.FormatVertex += VizFormatVertex;
+            return viz.Generate(new DotPrinter(), "");
+        }
+
+        private static void VizFormatVertex<TVertex>(object sender, FormatVertexEventArgs<TVertex> e)
+        {
+            e.VertexFormatter.Label = e.Vertex.ToString();
+        }
     }
 }
---
+++ Compilers/CFG/DotPrinter.cs
@@ -0,0 +1,11 @@
+﻿using QuickGraph.Graphviz;
+using QuickGraph.Graphviz.Dot;
+
+namespace SimpleLang.CFG
+{
+    public sealed class DotPrinter : IDotEngine
+    {
+        public string Run(GraphvizImageType imageType, string dot, string outputFileName) 
+            => dot;
+    }
+}
