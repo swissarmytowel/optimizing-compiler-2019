1523df269f9fd6d019ec6a1df10af987e3c00dda
aryazanova <aryazanova@gamenuts.ru>
2019-05-22 16:16:24 +0300

    Add UI of IDE

---
+++ IntegratedApp/MainForm.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace IntegratedApp
+{
+    partial class IntegratedApp
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InfoStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.TacItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CfgItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(758, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadStripMenuItem,
+            this.SaveStripMenuItem,
+            this.InfoStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // LoadStripMenuItem
+            // 
+            this.LoadStripMenuItem.Name = "LoadStripMenuItem";
+            this.LoadStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.LoadStripMenuItem.Text = "Загрузить";
+            this.LoadStripMenuItem.Click += new System.EventHandler(this.LoadStripMenuItem_Click);
+            // 
+            // SaveStripMenuItem
+            // 
+            this.SaveStripMenuItem.Name = "SaveStripMenuItem";
+            this.SaveStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.SaveStripMenuItem.Text = "Сохранить";
+            this.SaveStripMenuItem.Click += new System.EventHandler(this.SaveStripMenuItem_Click);
+            // 
+            // InfoStripMenuItem
+            // 
+            this.InfoStripMenuItem.Name = "InfoStripMenuItem";
+            this.InfoStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.InfoStripMenuItem.Text = "Справка";
+            this.InfoStripMenuItem.Click += new System.EventHandler(this.InfoStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 343);
+            this.panel1.TabIndex = 11;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox3.Location = new System.Drawing.Point(141, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(234, 343);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Оптимизации";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Controls.Add(this.menuStrip2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(141, 343);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Меню";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.InputTextBox);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(375, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(383, 343);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputTextBox.Location = new System.Drawing.Point(3, 16);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(377, 324);
+            this.InputTextBox.TabIndex = 1;
+            this.InputTextBox.Text = "";
+            // 
+            // TacItem
+            // 
+            this.TacItem.Name = "TacItem";
+            this.TacItem.Size = new System.Drawing.Size(128, 19);
+            this.TacItem.Text = "Трехадресный код";
+            this.TacItem.Click += new System.EventHandler(this.TacItem_Click);
+            // 
+            // CfgItem
+            // 
+            this.CfgItem.Name = "CfgItem";
+            this.CfgItem.Size = new System.Drawing.Size(128, 19);
+            this.CfgItem.Text = "CFG";
+            this.CfgItem.Click += new System.EventHandler(this.CfgItem_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TacItem,
+            this.CfgItem});
+            this.menuStrip2.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
+            this.menuStrip2.Location = new System.Drawing.Point(3, 16);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(135, 324);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(3, 3);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 23);
+            this.RunButton.TabIndex = 3;
+            this.RunButton.Text = "Запуск";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ClearButton);
+            this.panel2.Controls.Add(this.RunButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(3, 282);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(135, 58);
+            this.panel2.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBox4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBox3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(228, 324);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.CheckOnClick = true;
+            this.checkedListBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5"});
+            this.checkedListBox3.Location = new System.Drawing.Point(117, 3);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.ScrollAlwaysVisible = true;
+            this.checkedListBox3.Size = new System.Drawing.Size(108, 156);
+            this.checkedListBox3.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5",
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5",
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5"});
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.ScrollAlwaysVisible = true;
+            this.checkedListBox1.Size = new System.Drawing.Size(108, 156);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5"});
+            this.checkedListBox2.Location = new System.Drawing.Point(3, 165);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(108, 156);
+            this.checkedListBox2.TabIndex = 4;
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.CheckOnClick = true;
+            this.checkedListBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "опт1",
+            "опт2",
+            "опт3",
+            "опт4",
+            "опт5"});
+            this.checkedListBox4.Location = new System.Drawing.Point(117, 165);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(108, 156);
+            this.checkedListBox4.TabIndex = 3;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(3, 32);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // IntegratedApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(758, 367);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "IntegratedApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IntegratedApp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem LoadStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem InfoStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox InputTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem TacItem;
+        private System.Windows.Forms.ToolStripMenuItem CfgItem;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ IntegratedApp/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntegratedApp
+{
+    public partial class IntegratedApp : Form
+    {
+        public IntegratedApp()
+        {
+            InitializeComponent();
+        }
+
+#region callback StripMenuItems
+        private void LoadStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                InputTextBox.Text = sr.ReadToEnd();
+                sr.Close();
+                InputTextBox.SelectionStart = InputTextBox.Text.Length;
+            }
+        }
+
+        private void SaveStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                StreamWriter sw = new StreamWriter(saveFileDialog.FileName);
+                sw.WriteLine(InputTextBox.Text);
+                sw.Close();
+            }
+        }
+
+        private void InfoStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdditionalWindow tacWindow = new AdditionalWindow("Справка");
+            tacWindow.Show();
+        }
+        #endregion
+
+        #region callback Buttons
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("TO DO");
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Clear();
+        }
+        #endregion
+        
+        #region callback Windows
+        private void TacItem_Click(object sender, EventArgs e)
+        {
+            AdditionalWindow tacWindow = new AdditionalWindow("TAC Window");
+            tacWindow.Show();
+        }
+
+        private void CfgItem_Click(object sender, EventArgs e)
+        {
+            AdditionalWindow cfgWindow = new AdditionalWindow("CFG Window");
+            cfgWindow.Show();
+        }
+        #endregion
+
+    }
+}
