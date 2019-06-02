b7127b8c001ac4db550fba5c7c349e95a2398708
Golubev Gleb <embrace9624@gmail.com>
2019-06-02 20:56:51 +0300

    add common iteration alg and some his children

---
+++ Compilers/IterationAlgorithms/ReachingDefenitionsITA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleLang.TACode.TacNodes;
+using SimpleLang.TACode;
+using SimpleLang.CFG;
+using SimpleLang.GenKill.Interfaces;
+using SimpleLang.GenKill.Implementations;
+
+namespace SimpleLang.IterationAlgorithms
+{
+    class ReachingDefenitionsITA : IterationAlgorithm
+    {
+        protected override HashSet<TacNode> CollectionOperator(HashSet<TacNode> x, HashSet<TacNode> y)
+        {
+            return new HashSet<TacNode>(x.Union(y));
+        }
+
+        public ReachingDefenitionsITA(
+            ControlFlowGraph cfg,
+            Dictionary<ThreeAddressCode, IExpressionSetsContainer> lines) : base(cfg, new TFByComposition(lines))
+        {
+            Execute();
+        }
+    }
+}
