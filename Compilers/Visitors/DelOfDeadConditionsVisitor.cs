2ff5cc360cf86934ccc0635db32c2e308b70b4db
lgrw <47795679+lgrw@users.noreply.github.com>
2019-06-09 03:29:38 +0300

    Update and rename ThirteenthOptimizationVisitor.cs to DelOfDeadConditionsVisitor.cs

--- Compilers/Visitors/DelOfDeadConditionsVisitor.cs
+++ Compilers/Visitors/DelOfDeadConditionsVisitor.cs
@@ -2,7 +2,7 @@
 
 namespace SimpleLang.Visitors
 {
-    class ThirteenthOptimizationVisitor : ChangeVisitor
+    class DelOfDeadConditionsVisitor : ChangeVisitor
     {
         public override void VisitIfNode(IfNode c)
         {
