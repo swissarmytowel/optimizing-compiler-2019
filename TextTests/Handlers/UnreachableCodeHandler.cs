ac07e74f35da3accac9be7b844bdb4ec67e50e8d
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2019-06-09 19:23:12 +0300

    Add tests for unreachable code

---
+++ TextTests/Handlers/UnreachableCodeHandler.cs
@@ -0,0 +1,16 @@
+﻿using SimpleLang.Optimizations;
+using SimpleLang.TACode;
+
+namespace TextTests.Handlers
+{
+    public class UnreachableCodeHandler : TextTestsHandler
+    {
+        public UnreachableCodeHandler(string directoryName) : base(directoryName) { }
+        protected override ThreeAddressCode ProcessTAC(ThreeAddressCode tacContainer)
+        {
+            var optimization = new UnreachableCodeOpt();
+            optimization.Optimize(tacContainer);
+            return tacContainer;
+        }
+    }
+}
