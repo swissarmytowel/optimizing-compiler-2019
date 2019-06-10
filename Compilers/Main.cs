ba7c62f182567e6348565a822ebda6302707ea39
sergeyzin <sergeyzin1996@gmail.com>
2019-06-10 14:02:10 +0300

    Minor changes

--- Compilers/Main.cs
+++ Compilers/Main.cs
@@ -29,7 +29,8 @@ namespace SimpleCompiler
         public static void Main()
         {
             var DirectoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            string FileName = Path.Combine(DirectoryPath, "a1.txt");
+            string FileName = Path.Combine(DirectoryPath, "a.txt");
+
             try
             {
                 string Text = File.ReadAllText(FileName);
