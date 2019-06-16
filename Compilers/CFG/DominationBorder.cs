f281c390799d9ccba3b8c3148415e13e50098ad2
misi0ner <21069889ara>
2019-06-16 19:42:35 +0300

    dominator border

---
+++ Compilers/CFG/DominationBorder.cs
@@ -0,0 +1,33 @@
+﻿using SimpleLang.CFG.DominatorsTree;
+using SimpleLang.TACode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang.CFG
+{
+    public static class DominationBorder
+    {
+        public static HashSet<ThreeAddressCode> Execute(ControlFlowGraph cfg, ThreeAddressCode n)
+        {
+            var res = new HashSet<ThreeAddressCode>();
+            var dominatorsFinder = new DominatorsFinder(cfg);
+            var Pred = cfg.InEdges(n).Select(edge => edge.Source);
+            var iDomN = dominatorsFinder.ImmediateDominators[n];
+            if (Pred.Count() > 1)
+            {
+                foreach(var p in Pred)
+                {
+                    var r = p;
+                    while(r != iDomN)
+                    {
+                        res.Add(r);
+                        r = dominatorsFinder.ImmediateDominators[r];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
